45f265c7fd3e880cd4fca2e056dfb6558168274b
Simon Hansson <siimonhansson@gmail.com>
2025-12-02 10:44:19 +0100

    General Code Structure
    Added in the general code structure and reformatted GetInput as I realized there's only 1 input per day.

--- Program.cs
+++ Program.cs
@@ -9,15 +9,39 @@
 
         }
 
-        private static string GetInput(int day, int part)
+        private static string GetInput(int day)
         {
             string dayString = day.ToString("00");
-            string inputFilePath = Path.Combine(InputDir, $"Day_{dayString}_{part}.txt");
+            string inputFilePath = Path.Combine(InputDir, $"Day_{dayString}.txt");
             
             if (!File.Exists(inputFilePath)) 
                 throw new FileNotFoundException($"Input file {inputFilePath} not found");
             
             return File.ReadAllText(inputFilePath);
+        }
+
+        private static void SolveDay(int day)
+        {
+            Console.WriteLine($"~*~ Day {day:00} ~*~\n");
+            string input = GetInput(day);
+            Console.WriteLine($"Part 1: {SolvePart1(1, input)}");
+            Console.WriteLine($"Part 2: {SolvePart1(1, input)}\n");
+        }
+
+        private static string SolvePart1(int day, string input)
+        {
+            return day switch
+            {
+                _ => "Not Implemented"
+            };
+        }
+
+        private static string SolvePart2(int day, string input)
+        {
+            return day switch
+            {
+                _ => "Not Implemented"
+            };
         }
     }
 }
