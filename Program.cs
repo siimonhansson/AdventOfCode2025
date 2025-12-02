a6ee2b14a36b88108cbf0d0fef4548e59e612fa9
Simon Hansson <siimonhansson@gmail.com>
2025-12-02 10:28:33 +0100

    Get Input

--- Program.cs
+++ Program.cs
@@ -2,9 +2,22 @@
 {
     internal class Program
     {
+        private const string InputDir = @"D:\Annat\AdventOfCode2025\Input";
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+        }
+
+        private static string GetInput(int day, int part)
+        {
+            string dayString = day.ToString("00");
+            string inputFilePath = Path.Combine(InputDir, $"Day_{dayString}_{part}.txt");
+            
+            if (!File.Exists(inputFilePath)) 
+                throw new FileNotFoundException($"Input file {inputFilePath} not found");
+            
+            return File.ReadAllText(inputFilePath);
         }
     }
 }
