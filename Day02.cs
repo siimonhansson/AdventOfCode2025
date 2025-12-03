baafc57f1feead2138b4394c70d677e0c4f31429
Simon Hansson <siimonhansson@gmail.com>
2025-12-03 13:45:40 +0100

    Day 2 Part 1

--- Day02.cs
+++ Day02.cs
@@ -8,12 +8,43 @@ namespace AdventOfCode2025
     {
         public static string Part1(string input)
         {
-            return "Not implemented";
+            string[] inputLines = input.Split(',');
+            long total = 0;
+
+            foreach (string line in inputLines)
+            {
+                string trimmedLine = line.Trim();
+                
+                if (string.IsNullOrEmpty(trimmedLine)) continue;
+
+                string[] parts = trimmedLine.Split('-');
+                long start = long.Parse(parts[0]);
+                long end = long.Parse(parts[1]);
+
+                for (long i = start; i <= end; i++)
+                {
+                    if (!IsValidID(i)) total += i;
+                }
+            }
+
+            return total.ToString();
         }
 
         public static string Part2(string input)
         {
             return "Not implemented";
         }
+
+        private static bool IsValidID(long id)
+        {
+            string idString = id.ToString();
+            int idLen = idString.Length;
+
+            if (idLen % 2 != 0) return true; // Can't be mirrored if not even.
+
+            int midPoint = idLen / 2;
+
+            return idString[..midPoint] != idString[midPoint..]; // Invalid if mirrored.
+        } 
     }
 }
